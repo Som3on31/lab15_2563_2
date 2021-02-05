69122686fdba2a33965270d2124c4e79ecafa596
Som3on31 <Som3on31_kek@users.noreply.github.com>
2021-02-05 17:07:54 +0700

    Completed the source code.

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,37 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *array,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << array[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *array,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        (double &)array[i] = double (rand()%101)/100;
+    }
+}
+
+void findRowSum(const double *array,double *result,int N,int M){
+    for (int i=0;i<N;i++){
+        result[i]=0;
+        for (int j=0;j<M;j++){
+            result[i]+=array[(M*i)+j];
+        }
+    }
+}
+
+void findColSum(const double *array,double *result,int N,int M){
+    for (int i=0;i<M;i++){
+        result[i]=0;
+        for (int j=0;j<N;j++){
+            result[i]+=array[(M*j)+i];
+        }
+    }
+}
+
